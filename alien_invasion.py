970be9961ef593641feb024e6a02503b72f1cea8
Stephen Porter <sigsporter@gmail.com>
2023-12-11 17:57:12 -0600

    Created _update_bullets helper function & cleaned up run_game

--- alien_invasion.py
+++ alien_invasion.py
@@ -26,14 +26,7 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
-            self.bullets .update()
-
-            # Remove bullets that have left the screen.
-            for bullet in self.bullets.copy():
-                if bullet.rect.bottom <= 0:
-                    self.bullets.remove(bullet)
-            print(len(self.bullets))
-
+            self._update_bullets()
             self._update_screen()
 
     def _check_events(self):
@@ -66,8 +59,20 @@ class AlienInvasion:
 
     def _fire_bullet(self):
         """Create a new bullet and add it to the bullets group."""
-        new_bullet = Bullet(self)
-        self.bullets.add(new_bullet)
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+        """Update position of bullets and remove offscreen bullets"""
+        # Update bullet positions
+        self.bullets.update()
+
+        # Remove bullets that have left the screen.
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+
 
     def _update_screen(self):
         """Update images on the scree, and flip to the new screen."""
