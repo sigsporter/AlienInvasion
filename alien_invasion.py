365364d2799d5a6d53f755dcea776367a4df989c
Stephen Porter <sigsporter@gmail.com>
2023-12-13 18:24:38 -0600

    Creates new fleet when previous fleet is cleared.

--- alien_invasion.py
+++ alien_invasion.py
@@ -83,6 +83,11 @@ class AlienInvasion:
         # Check for any bullets that have hit aliens. If so, get rid of the bullet and alien.
         collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
 
+        if not self.aliens:
+            # Destroy existing bullets and create new fleet.
+            self.bullets.empty()
+            self._create_fleet()
+
     def _create_fleet(self):
         """Create the fleet of aliens"""
         # Create an alien and find the number of aliens in a row. Spacing b'n each alien is equal to one alien width.
