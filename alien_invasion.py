8fa35d8d26998c9901ebecc7c7a4d32faadb6452
Stephen Porter <sigsporter@gmail.com>
2023-12-13 18:47:57 -0600

    Created game stats file to track statistics. Added ship_limit to ship class. Created functions to handle when a ship is hit to reset the game and reduce ships_left by 1.

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,4 +1,5 @@
 import sys
+from time import sleep
 
 import pygame
 
@@ -6,6 +7,7 @@ from settings import Settings
 from ship import Ship
 from bullet import Bullet
 from alien import Alien
+from game_stats import GameStats
 
 
 class AlienInvasion:
@@ -20,6 +22,9 @@ class AlienInvasion:
         self.settings.screen_width = self.screen.get_rect().width
         self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion")
+
+        # Create an instance to store game statistics.
+        self.stats = GameStats(self)
 
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
@@ -125,8 +130,12 @@ class AlienInvasion:
         self._check_fleet_edges()
         self.aliens.update()
 
+        # Look for alien-ship collisions.
+        if pygame.sprite.spritecollideany(self.ship, self.aliens):
+            self._ship_hit()
+
     def _check_fleet_edges(self):
-        """Respond approrpately if any aliens ahve reached the edge."""
+        """Respond appropriately if any aliens have reached the edge."""
         for  alien in self.aliens.sprites():
             if alien.check_edges():
                 self._change_fleet_direction()
@@ -137,6 +146,23 @@ class AlienInvasion:
         for alien in self.aliens.sprites():
             alien.rect.y += self.settings.fleet_drop_speed
         self.settings.fleet_direction *= -1
+
+    def _ship_hit(self):
+        """Respond to the ship being hit by an alien."""
+
+        # Decrement ships left
+        self.stats.ship_left -= 1
+
+        # Get rid of any remaining bullets and aliens.
+        self.aliens.empty()
+        self.bullets.empty()
+
+        # Create a new fleet and center the ship.
+        self._create_fleet()
+        self.ship.center_ship()
+
+        # Pause
+        sleep(0.5)
 
     def _update_screen(self):
         """Update images on the scree, and flip to the new screen."""
